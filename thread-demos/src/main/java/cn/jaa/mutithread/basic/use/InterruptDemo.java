c6d09c73476022199990046ae72598b8d778619f
fenng <xqjia123@gmail.com>
2024-04-06 21:24:17 +0800

    thread interrupt2 demo

--- thread-demos/src/main/java/cn/jaa/mutithread/basic/use/InterruptDemo.java
+++ thread-demos/src/main/java/cn/jaa/mutithread/basic/use/InterruptDemo.java
@@ -1,7 +1,9 @@
 package cn.jaa.mutithread.basic.use;
 
 import cn.jaa.util.Print;
+import org.junit.Test;
 
+import static cn.jaa.util.ThreadUtil.sleepMilliSeconds;
 import static cn.jaa.util.ThreadUtil.sleepSeconds;
 
 /**
@@ -51,5 +53,32 @@ public class InterruptDemo {
 
         sleepSeconds(1);        // 等待1秒
         Print.tco("程序运行结束.");
+    }
+
+    /**
+     * 测试用例：获取异步调用的结果
+     */
+    @Test
+    public void testInterrupted2() {
+        Thread thread = new Thread() {
+            public void run() {
+                Print.tco("线程启动了");
+                // 一直循环
+                while (true) {
+                    Print.tco(isInterrupted());
+                    sleepMilliSeconds(SLEEP_GAP);
+                    if (isInterrupted()) {
+                        Print.tco("线程结束了");
+                        return;
+                    }
+                }
+            }
+        };
+
+        thread.start();
+        sleepSeconds(2);    // 等待2秒
+        thread.interrupt(); // 打断线程1
+        sleepSeconds(2);    // 等待2秒
+        thread.interrupt();
     }
 }
