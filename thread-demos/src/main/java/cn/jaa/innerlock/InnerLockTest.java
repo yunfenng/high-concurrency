f58a39f0402536641c64f8149f6b179e24575fe7
fenng <xqjia123@gmail.com>
2024-04-09 22:57:26 +0800

    Java内置锁-无锁、偏向锁测试

---
+++ thread-demos/src/main/java/cn/jaa/innerlock/InnerLockTest.java
@@ -0,0 +1,74 @@
+package cn.jaa.innerlock;
+
+import cn.jaa.util.Print;
+import org.junit.Test;
+import org.openjdk.jol.vm.VM;
+
+import java.util.concurrent.CountDownLatch;
+
+import static cn.jaa.util.ThreadUtil.sleepMilliSeconds;
+
+/**
+ * @Author: Jaa
+ * @Description: 对象布局分析的用例代码
+ * @Date 2024/4/9
+ */
+public class InnerLockTest {
+
+    final int MAX_TREAD = 10;
+    final int MAX_TURN = 1000;
+    CountDownLatch latch = new CountDownLatch(MAX_TREAD);
+
+    /**
+     * 测试无锁对象
+     */
+    @Test
+    public void showNoLockObject() throws InterruptedException {
+        // 输出JVM的信息
+        Print.fo(VM.current().details());
+        // 创建一个对象
+        ObjectLock objectLock = new ObjectLock();
+        Print.fo("object status: ");
+        // 输出对象的布局信息
+        objectLock.printSelf();
+    }
+
+    /**
+     * 偏向锁案例演示
+     */
+    @Test
+    public void showBiasedLock() throws InterruptedException {
+        Print.tcfo(VM.current().details());
+        // JVM延迟偏向锁
+        sleepMilliSeconds(5000);
+
+        ObjectLock lock = new ObjectLock();
+        Print.tcfo("抢占锁前, lock 的状态: ");
+        lock.printObjectStruct();
+
+        sleepMilliSeconds(5000);
+        CountDownLatch latch = new CountDownLatch(1);
+
+        Runnable runnable = () -> {
+            for (int i = 0; i < MAX_TURN; i++) {
+                synchronized (lock) {
+                    lock.increase();
+                    if (i == MAX_TURN / 2) {
+                        Print.tcfo("占有锁, lock 的状态: ");
+                        lock.printObjectStruct();
+                        // 读取字符串型输入,阻塞线程
+                        // Print.consoleInput();
+                    }
+                }
+                // 每一次循环等待10ms
+                sleepMilliSeconds(10);
+            }
+            latch.countDown();
+        };
+        new Thread(runnable, "biased-demo-thread").start();
+        // 等待加锁线程执行完成
+        latch.await();
+        Print.tcfo("释放锁后, lock 的状态: ");
+        lock.printObjectStruct();
+    }
+}
