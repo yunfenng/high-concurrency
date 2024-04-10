75a52968bdd7915093a686ceb94f48c8b87e4962
fenng <xqjia123@gmail.com>
2024-04-10 16:03:01 +0800

    Java内置锁-轻量级锁测试

--- thread-demos/src/main/java/cn/jaa/innerlock/InnerLockTest.java
+++ thread-demos/src/main/java/cn/jaa/innerlock/InnerLockTest.java
@@ -71,4 +71,66 @@ public class InnerLockTest {
         Print.tcfo("释放锁后, lock 的状态: ");
         lock.printObjectStruct();
     }
+
+    /**
+     * 轻量级锁案例演示
+     */
+    @Test
+    public void showLightweightLock() throws InterruptedException {
+        Print.tcfo(VM.current().details());
+        // JVM延迟偏向锁
+        sleepMilliSeconds(5000);
+
+        ObjectLock lock = new ObjectLock();
+
+        Print.tcfo("抢占锁前, lock 的状态: ");
+        lock.printObjectStruct();
+
+        sleepMilliSeconds(5000);
+        CountDownLatch latch = new CountDownLatch(2);
+        Runnable runnable = () -> {
+            for (int i = 0; i < MAX_TURN; i++) {
+                synchronized (lock) {
+                    lock.increase();
+                    if (i == 1) {
+                        Print.tcfo("第一个线程占有锁, lock 的状态: ");
+                        lock.printObjectStruct();
+                    }
+                }
+            }
+            //循环完毕
+            latch.countDown();
+
+            // 线程虽然释放锁，但是一直存在
+            for (int j = 0; ; j++) {
+                // 每一次循环等待1ms
+                sleepMilliSeconds(1);
+            }
+        };
+        new Thread(runnable).start();
+
+        sleepMilliSeconds(1000); //等待1s
+
+        Runnable lightweightRunnable = () -> {
+            for (int i = 0; i < MAX_TURN; i++) {
+                synchronized (lock) {
+                    lock.increase();
+                    if (i == MAX_TURN / 2) {
+                        Print.tcfo("第二个线程占有锁, lock 的状态: ");
+                        lock.printObjectStruct();
+                    }
+                    // 每一次循环等待1ms
+                    sleepMilliSeconds(1);
+                }
+            }
+            // 循环完毕
+            latch.countDown();
+        };
+        new Thread(lightweightRunnable).start();
+        // 等待加锁线程执行完成
+        latch.await();
+        sleepMilliSeconds(2000);  // 等待2s
+        Print.tcfo("释放锁后, lock 的状态: ");
+        lock.printObjectStruct();
+    }
 }
